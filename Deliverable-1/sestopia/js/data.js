8f40a715fb59282a14e54c517bddf5045dd1c3cf
harshdivecha <harshdivecha@outlook.com>
2020-07-25 22:51:43 -0400

    style sheet trial

--- Deliverable-1/sestopia/js/data.js
+++ Deliverable-1/sestopia/js/data.js
@@ -83,46 +83,91 @@ const content = [
 	title: `Software Maintenance`,
 	description: `<hr border-top: 3px solid #bbb;></hr><br>Lorem4 ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dictum condimentum. Donec aliquam libero eu malesuada aliquam. Aliquam erat volutpat. Proin eu velit ut augue rutrum vestibulum. `,
 	text: `
-	<h4> Name of Skill </h4>
-	Reengineering
-
-	<h4> Classification of Skill </h4>
-	Technical
-
-	<h4> Prerequisites for Skill </h4>
-	Programming  
-
-	<h4> Related Software Engineering Area(s)  </h4>
-	Software Quality,Software Testing,Software Configuration Management
+<div class="sm">
+		<h2  class="sm"> Name of Skill </h2>
+		Reengineering
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> Rationale for Skill </h4><h4> Roles for Skill </h4>
-	Requires skill to understand code written by others, helps to increase efficiency of the code.
+	<div class="sm">
+		<h2  class="sm"> Classification of Skill </h2>
+		Technical
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> Work Related to Skill (Related Activities and Artifacts) </h4>
-	Refactor, Reverse eng, code restructuring, forward eng,redocumentation
+	<div class="sm">
+		<h2  class="sm"> Prerequisites for Skill </h2>
+		Programming  
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> Real-World Example/Scenario of Skill (Text, Graphic, Audio, Video) </h4>
-	taking a legacy sofware and adapting to mordern systems.
+	<div class="sm">
+		<h2  class="sm"> Related Software Engineering Area(s)  </h2>
+		Software Quality,Software Testing,Software Configuration Management
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> Role of Academia or Industry in Cultivating the Skill </h4>
-
-	<h4> Tools Supporting the Skill </h4>
-	Lighthouse
-	Dependency analyzer
-	Disassembler
-	PE and HEX editors
+	<div class="sm">
+		<h2  class="sm"> Rationale for Skill </h2>
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> Skill Self-Assessment (My Skill Score (1 – 10) and Reasons for Self-Assigned Score) </h4>
-	5
+	<div class="sm"><h2  class="sm"> Roles for Skill </h2>
+		Requires skill to understand code written by others, helps to increase efficiency of the code.
+		<hr class="smSolid" ></hr>
+	</div>
 
 
-	<h4> References </h4>
-	`  },
+	<div class="sm">
+		<h2  class="sm"> Work Related to Skill (Related Activities and Artifacts) </h2>
+		Refactor, Reverse eng, code restructuring, forward eng,redocumentation
+		<hr class="smSolid" ></hr>
+	</div>
+
+
+	<div class="sm">
+		<h2  class="sm"> Real-World Example/Scenario of Skill (Text, Graphic, Audio, Video) </h2>
+		taking a legacy sofware and adapting to mordern systems.
+		<hr class="smSolid" ></hr>
+	</div>
+
+
+	<div class="sm">
+		<h2  class="sm"> Role of Academia or Industry in Cultivating the Skill </h2>
+		<hr class="smSolid" ></hr>
+	</div>
+
+
+	<div class="sm">
+		<h2  class="sm"> Tools Supporting the Skill </h2>
+		<ul class="sm">
+			<li>Lighthouse</li>
+			<li>Dependency analyzer</li>
+			<li>Disassembler</li>
+			<li>PE and HEX editors</li>
+		</ul>
+		<hr class="smSolid" ></hr>
+	</div>
+
+
+	<div class="sm">
+		<h2  class="sm"> Skill Self-Assessment (My Skill Score (1 – 10) and Reasons for Self-Assigned Score) </h2>
+		5
+		<hr class="smSolid" ></hr>
+	</div>
+
+
+	<div class="sm">
+		<h2  class="sm"> References </h2>
+		<hr class="smSolid" ></hr>
+	</div>	
+`  
+},
 	{
 		title: `Software Engineering Process`,
 		description: `Lorem5 ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dictum condimentum. Donec aliquam libero eu malesuada aliquam. Aliquam erat volutpat. Proin eu velit ut augue rutrum vestibulum. `,

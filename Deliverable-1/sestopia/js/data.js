16ea53c0c10be181e5327dafb541a75e6f50b4c9
nehadighe <neha.dighe18@gmail.com>
2020-07-26 20:24:11 -0400

    Update data.js
    Review and audite skill - QA implementation by Neha

--- Deliverable-1/sestopia/js/data.js
+++ Deliverable-1/sestopia/js/data.js
@@ -77,7 +77,126 @@ const content = [
 {
 	title: `Software Quality`,
 	description: `Lorem3 ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dictum condimentum. Donec aliquam libero eu malesuada aliquam. Aliquam erat volutpat. Proin eu velit ut augue rutrum vestibulum. `,
-	text: `<p>Lorem1 ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dictum condimentum. Donec aliquam libero eu malesuada aliquam. Aliquam erat volutpat. Proin eu velit ut augue rutrum vestibulum. Maecenas sit amet nisl arcu. Aenean sagittis tempor felis, at facilisis nulla mattis vel. Duis sed magna cursus, pharetra enim a, vehicula sem. Phasellus velit ex, porttitor quis tempor in, congue sed felis. Aliquam accumsan tellus ac lectus porta dictum. Duis imperdiet, purus id ultricies ullamcorper, quam nibh dictum felis, sit amet volutpat enim arcu et lacus. In eget massa feugiat metus laoreet varius. Praesent finibus eget nunc ut finibus. Praesent vitae tincidunt orci. Pellentesque vulputate nisl sed nunc tempor blandit.</p><p><a href="./index.html">Return Home</a></p>`,
+	text:
+	`
+	
+	<hr style="solid 5px #bbb"></hr>
+	<div class="sm">
+		<h2  class="sm"> Name of Skill </h2>
+		Conducting Reviews and Audits
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Classification of Skill </h2>
+		Management Reviews, Technical Reviews, Inspections, Walkthroughs
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Prerequisites for Skill </h2>
+		Functional and Technical Understanding of the system  
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Related Software Engineering Area(s)  </h2>
+		Software Requirements,Software Testing
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Rationale for Skill </h2>
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm"><h2  class="sm"> Roles for Skill </h2>
+		 Decision maker (i.e., software lead), a Review leader, a recorder, and checkers (technical staff members who examine the work-products). 
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Work Related to Skill </h2>
+		Examination of software engineering artifacts with respect to standards that have been established by the organization or project for those artifacts.
+		
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Real-World Example </h2>
+		Management reviews evaluate actual project results with respect to plans.
+        Technical reviews (including inspections, walkthrough, and desk checking) examine engineering work-products.
+		
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Role of Academia or Industry in Cultivating the Skill </h2>
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm" > Tools Supporting the Skill </h2>
+		<div class="row" style="padding-top:70px">
+			
+			<div class="column">
+				<div class="card">
+					<h4>Phabricator</h4>
+					<p>Phabricator is a complete suite of open-source software development applications which include light-weight web-based code review, planning, testing, browsing and audit score, finding bugs, etc.
+
+					Key Features:
+
+					Code review tool from Phabricator suite is termed as “Differential”. It is used in minimizing the efforts required in creating the best quality code.
+					Phabricator has two types of code review workflows, namely “pre-push” also termed as “review” and “post-push” termed as “audit”.
+					Phabricator can be integrated with Git, Subversion, and Mercurial.</p>
+				</div>
+			</div>
+			<div class="column">
+				<div class="card">
+					<h4>Crucible</h4>
+					<p>Crucible is a web-based collaborative code review application used by developers for code review, finding defects, discussing the changes and knowledge sharing, etc.
+
+					Key Features:
+
+					Crucible is a flexible application that accommodates ample range of work approaches and team sizes.
+					Crucible is a lightweight peer code review tool that is used in pre-commit and post-commit reviews.
+					Code review has become easy for SVN, Perforce, and CVS etc using Crucible.</p>
+				</div>
+			</div>
+			<div class="column">
+				<div class="card">
+					<h4>Veracode</h4>
+					<p> Veracode (now acquired by CA Technologies) is a company which delivers various solutions for automated & on-demand application security testing, automated code review, etc.
+
+					Key Features:
+
+					Veracode is used by the developers in creating secured software by scanning the binary code or byte code in place of source code.
+					Using Veracode one can identify the improper encrypted functionalities, malicious code and backdoors from source code.
+					Veracode can review a large amount of code and returns the results immediately.
+					To use Veracode there is no need to buy any software or hardware, you just need to pay for the analysis services you need.</p>
+				</div>
+			</div>
+			<div class="column">
+				<div class="card">
+					<h4>Review Board</h4>
+					<p>Review Board is a web-based, collaborative, free and open-source tool used for code review and document review by open-source projects and companies.
+
+					Key Features:
+
+					Using Review Board for code review one can save money and time. Time saved can be used in concentrating on creating great software.
+					Review Board can be integrated with ClearCase, CVS, Perforce, Plastic, etc.
+					In a code review by Review Board tool, the code is syntax highlighted which makes it be read faster.
+					Review Board supports pre-commit reviews and post-commit reviews.</p>
+				</div>
+			</div>
+		</div>
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> Skill Self-Assessment </h2>
+		6
+		<hr class="smSolid" ></hr>
+	</div>
+	<div class="sm">
+		<h2  class="sm"> References </h2>
+		https://www.softwaretestinghelp.com/code-review-tools/
+		https://www.guru99.com/software-quality-assurance-test-audit-review-makes-your-life-easy.html
+		http://www.qasigma.com/2008/12/sqa-audit.html
+		<hr class="smSolid" ></hr>
+	</div>	
+`
+	,
 },
 {
 	title: `Software Maintenance`,

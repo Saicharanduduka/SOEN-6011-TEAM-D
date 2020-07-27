a4de025f157b2176ec0066e334a54fec4811a21a
harshdivecha <harshdivecha@outlook.com>
2020-07-27 15:07:29 -0400

    Basic structure

--- Deliverable-1/sestopia/js/data.js
+++ Deliverable-1/sestopia/js/data.js
@@ -1,6 +1,6 @@
 const pageNotFoundMessage = `
 <div class="col-10">
-<h2 class="pb-3">Page Not Found</h2>
+<h2 class="pb-3">Page Not Found</h2><hr style="solid 5px #bbb"></hr>
 <p>
 <a href="./index.html">Return Home</a>
 </p>
@@ -84,99 +84,150 @@ const content = [
 	description: `<hr border-top: 3px solid #bbb;></hr><br>Lorem4 ipsum dolor sit amet, consectetur adipiscing elit. Sed dictum dictum condimentum. Donec aliquam libero eu malesuada aliquam. Aliquam erat volutpat. Proin eu velit ut augue rutrum vestibulum. `,
 	text: `
 	
-	<hr style="solid 5px #bbb"></hr>
-	<div class="sm">
-		<h2  class="sm"> Name of Skill </h2>
+<h2>Index</h2>
+<div class="box" id="index">
+	<ul>
+		<li><a href="#nos">1. Name of Skill.</a></li>
+		<li><a href="#cos">2. Classification of Skill.</a></li>
+		<li><a href="#pfs">3. Prerequisites for Skill.</a></li>
+		<li><a href="#rsfea">4. Related Software Engineering Area(s).</a></li>
+		<li><a href="#rafs">5. Rationale for Skill. </a></li>
+		<li><a href="#rofs">6. Roles for Skill.</a></li>
+		<li><a href="#wrts">7. Work Related to Skill.</a></li>
+		<li><a href="#rwe">8. Real-World Example/Scenario of Skill.</a></li>
+		<li><a href="#roai">9. Role of Academia or Industry in Cultivating the Skill.</a></li>
+		<li><a href="#tsts">10. Tools Supporting the Skill.</a></li>
+		<li><a href="#ssa">11. Skill Self-Assessment.</a></li>
+		<li><a href="#r">12. References.</a></li>
+	</ul>	
+</div>
+
+
+	<div class="sm" id="nos">
+		<h2  class="sm"> <a href="#index"> Name of Skill</a> </h2><hr style="solid 5px #bbb"></hr>
+		
 		Reengineering
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Classification of Skill </h2>
+	<div class="sm" id="cos">
+		<h2  class="sm"> <a href="#index">Classification of Skill </a></h2>
+		<hr style="solid 5px #bbb"></hr>
 		Technical
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Prerequisites for Skill </h2>
+	<div class="sm" id="pfs">
+		<h2  class="sm"> <a href="#index">Prerequisites for Skill</a> </h2>
+		<hr style="solid 5px #bbb"></hr>
 		Programming  
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Related Software Engineering Area(s)  </h2>
-		Software Quality,Software Testing,Software Configuration Management
+	<div class="sm" id="rsfea">
+		<h2  class="sm"><a href="#index"> Related Software Engineering Area(s) </a> </h2>
+		<hr style="solid 5px #bbb"></hr>
+		<ul class="sm">
+			<li>Software Quality</li>
+			<li>Software Testing</li>
+			<li>Software Configuration Management</li>
+		</ul>
+
+		
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Rationale for Skill </h2>
+	<div class="sm" id="rafs">
+		<h2  class="sm"> <a href="#index">Rationale for Skill</a> </h2>
+		<hr style="solid 5px #bbb"></hr>
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm"><h2  class="sm"> Roles for Skill </h2>
+	<div class="sm" id="rofs">
+		<h2  class="sm"> <a href="#index">Roles for Skill </a></h2>
+		<hr style="solid 5px #bbb"></hr>
 		Requires skill to understand code written by others, helps to increase efficiency of the code.
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Work Related to Skill </h2>
-		Refactor, Reverse eng, code restructuring, forward eng,redocumentation
+	<div class="sm" id="wrts">
+		<h2  class="sm"> <a href="#index">Work Related to Skill </a></h2>
+		<hr style="solid 5px #bbb"></hr>
+		<ul class="sm">
+			<li>Refactor</li>
+			<li>Reverse eng</li>
+			<li>Code restructuring</li>
+			<li>Forward eng</li>
+			<li>Redocumentation</li>
+		</ul> 
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Real-World Example </h2>
+	<div class="sm" id="rwe">
+		<h2  class="sm"><a href="#index">Real-World Example</a></h2>
+		<hr style="solid 5px #bbb"></hr>
 		taking a legacy sofware and adapting to mordern systems.
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Role of Academia or Industry in Cultivating the Skill </h2>
+	<div class="sm" id="roai">
+		<h2  class="sm"> <a href="#index">Role of Academia or Industry in Cultivating the Skill</a> </h2>
+		<hr style="solid 5px #bbb"></hr>
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm" > Tools Supporting the Skill </h2>
+	<div class="sm" id="tsts">
+		<h2  class="sm" > <a href="#index">Tools Supporting the Skill </a></h2>
+		<hr style="solid 5px #bbb"></hr>
 
-		<div class="row" style="padding-top:70px">
+		<div class="row" style="padding-top:50px">
 			
 			<div class="column">
+				<a href="">
 				<div class="card">
 					<h4>Lighthouse</h4>
 					<p>Description</p>
 				</div>
+					
+				</a>
 			</div>
 
 			<div class="column">
-				<div class="card">
+				<a href="">
+					<div class="card">
 					<h4>Dependency analyzer</h4>
 					<p>Description</p>
 				</div>
+
+				</a>
 			</div>
 
 			<div class="column">
-				<div class="card">
-					<h4>Disassembler</h4>
-					<p>Description</p>
-				</div>
+				<a href="">
+					<div class="card">
+						<h4>Disassembler</h4>
+						<p>Description</p>
+					</div>					
+				</a>
 			</div>
 
 
 			<div class="column">
+				<a href="">
 				<div class="card">
 					<h4>PE and HEX editors</h4>
 					<p>Description</p>
-				</div>
+				</div>					
+				</a>
 			</div>
 
 		</div>
@@ -185,15 +236,17 @@ const content = [
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> Skill Self-Assessment </h2>
+	<div class="sm" id="ssa">
+		<h2  class="sm"><a href="#index"> Skill Self-Assessment</a> </h2>
+		<hr style="solid 5px #bbb"></hr>
 		5
 		<hr class="smSolid" ></hr>
 	</div>
 
 
-	<div class="sm">
-		<h2  class="sm"> References </h2>
+	<div class="sm" id="r">
+		<h2  class="sm"> <a href="#index">References </a></h2>
+		<hr style="solid 5px #bbb"></hr>
 		<hr class="smSolid" ></hr>
 	</div>	
 `  

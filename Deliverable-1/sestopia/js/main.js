9a456b6dafa21b04c72f342a73ab8d6ded5daef1
Yash Golwala <golwalayash@gmail.com>
2020-08-04 13:39:13 -0400

    Content page Navbar functionality JS

--- Deliverable-1/sestopia/js/main.js
+++ Deliverable-1/sestopia/js/main.js
@@ -115,3 +115,18 @@ function displayResults(results) {
 
   mainContent.innerHTML = html;
 }
+
+/**
+ * Navbar while user is scrolling down a page and wish to go back to the table of contents or home page.
+ * When the user scrolls down 20px from the top of the document, slide down the navbar
+ * When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
+ */
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar_pa").style.top = "0";
+  } else {
+    document.getElementById("navbar_pa").style.top = "-100px";
+  }
+}

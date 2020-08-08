34d47a8c25effcd1b949151d708f4690ed7c8b14
rafaelbf26 <31141157+rafaelbf26@users.noreply.github.com>
2020-08-08 16:46:38 -0400

    adding php files attempt

---
+++ Deliverable-1/sestopia/DB Scripts/skills.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `epiz_26274222_Sestopia_db`.`skills` ( `title` VARCHAR(50) NOT NULL , `description` VARCHAR(50) NULL , `text` VARCHAR(9999) NOT NULL , `id` INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (`id`)) ENGINE = MyISAM;

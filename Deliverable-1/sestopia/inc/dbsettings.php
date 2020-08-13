eb3699a250f3bc1012803ad0c1a409cc3858daba
Sara <sarehfarid@gmail.com>
2020-08-13 20:41:03 +0430

    Updated DB content based on data.js

--- Deliverable-1/sestopia/inc/dbsettings.php
+++ Deliverable-1/sestopia/inc/dbsettings.php
@@ -4,15 +4,15 @@
 require_once("vendor/autoload.php");
 
 // MEEKRO - database variables SERVER
-DB::$host = "sql102.epizy.com";
-DB::$user = "epiz_26274222";
-DB::$password = "C4xE5g2wuWzy";
-DB::$dbName = "epiz_26274222_Sestopia_db";
+DB::$host = "";
+DB::$user = "";
+DB::$password = "";
+DB::$dbName = "";
 DB::$encoding = "utf8";
 
 // MEEKRO - database variables LOCALHOST
 //DB::$host = "localhost";
 //DB::$user = "root";
 //DB::$password = "";
-//DB::$dbName = "epiz_26274222_Sestopia_db";
+//DB::$dbName = "Sestopia_db";
 //DB::$encoding = "utf8";
